a560b25d3c3254c270ef277af8a3ceb092159ea2
Ivan Dashkevich <idashkevich@lement.pro>
2018-10-16 16:15:16 +0300

    Revert "Рефакторинг: публичный интерфейс"
    This reverts commit 08dad86742da0ac789180178c3f7455823d9808d.

--- utils/navigation.js
+++ utils/navigation.js
@@ -1,48 +1,52 @@
-module.exports = {
-  buildFolderUrl: (parentHash, path = '') => {
-    return `/files/${parentHash}/${path}`;
-  },
+function buildFolderUrl(parentHash, path = '') {
+  return `/files/${parentHash}/${path}`;
+}
 
-  buildFileUrl: (parentHash, path) => {
-    return `/content/${parentHash}/${path}`;
-  },
+function buildFileUrl(parentHash, path) {
+  return `/content/${parentHash}/${path}`;
+}
 
-  buildBreadcrumbs: (hash, path) => {
-    const bc = [
-      {
-        text: 'HISTORY',
-        href: hash ? '/' : undefined
-      }
-    ];
-  
-    if (hash) {
-      const normalizedPath = (path || '').split('/').filter(Boolean);
-      const [currentName] = normalizedPath.slice(-1);
-  
-      // root folder
-      bc.push({
-        text: 'ROOT',
-        href: normalizedPath.length ? `/files/${hash}/` : undefined
-      });
-  
-      // path
-      let fullPath = '';
-      for (let i = 0; i < normalizedPath.length - 1; i++) {
-        const part = normalizedPath[i];
-        fullPath += `${part}/`;
-        bc.push({
-          text: part,
-          href: `/files/${hash}/${fullPath}`
-        });
-      }
-  
-      // last part
-      currentName &&
-        bc.push({
-          text: currentName
-        });
+function buildBreadcrumbs(hash, path) {
+  const bc = [
+    {
+      text: 'HISTORY',
+      href: hash ? '/' : undefined
     }
-  
-    return bc;
+  ];
+
+  if (hash) {
+    const normalizedPath = (path || '').split('/').filter(Boolean);
+    const [currentName] = normalizedPath.slice(-1);
+
+    // root folder
+    bc.push({
+      text: 'ROOT',
+      href: normalizedPath.length ? `/files/${hash}/` : undefined
+    });
+
+    // path
+    let fullPath = '';
+    for (let i = 0; i < normalizedPath.length - 1; i++) {
+      const part = normalizedPath[i];
+      fullPath += `${part}/`;
+      bc.push({
+        text: part,
+        href: `/files/${hash}/${fullPath}`
+      });
+    }
+
+    // last part
+    currentName &&
+      bc.push({
+        text: currentName
+      });
   }
+
+  return bc;
+}
+
+module.exports = {
+  buildFolderUrl,
+  buildFileUrl,
+  buildBreadcrumbs
 };
