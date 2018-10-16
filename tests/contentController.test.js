890c95cf431753a3c4cfb9507738d35e94737ec6
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-10-17 01:17:48 +0300

    Testing [homework]: написал тест на render

--- tests/contentController.test.js
+++ tests/contentController.test.js
@@ -1,4 +1,25 @@
+const contentController = require('../controllers/contentController');
+const express = require('express');
+const request = require('supertest');
+const path = require('path');
+const expect = require('chai').expect;
+
 describe('contentController', () => {
-  it('тесты запускаются', () => {
+  it('возвращает корректные данные в шаблоне', (done) => {
+    const app = express();
+
+    app.set('views', path.join(__dirname, '../views'));
+    app.set('view engine', 'hbs');
+    app.set('view options', { layout: 'layout', extname: '.hbs' });
+    app.get('/content/:hash/*?', contentController);
+
+    request(app)
+      .get('/content/0a88cdf2265c0b19663ddbe2733a27e9599724e1/.gitignore')
+      .expect(200)
+      .expect((res) => {
+        expect(res.text).to.include('<div class="file-content">node_modules</div>')
+        done()
+      })
+      .catch(done)
   })
 })
