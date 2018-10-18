d8e9a0dc5e63ae85759601aa55ce4e1d146bda22
kauzlein <kauzlein@gmail.com>
2018-10-19 00:12:33 +0300

    Added integration tests

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -6,7 +6,12 @@ module.exports = {
         chrome: {
             desiredCapabilities: {
                 browserName: 'chrome'
-            }
+            },
+            // firefox: {
+            //     desiredCapabilities: {
+            //         browserName: 'firefox'
+            //     }
+            // }
         }
     },
     plugins: {
