1ac31291b9ca4903fb970c8c73057118ff4e79c9
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-10-19 02:35:12 +0300

    Testing [homework]: написал hermione тесты на главную страницу

--- tests/integration/commitsHistory.hermione.js
+++ tests/integration/commitsHistory.hermione.js
@@ -1,30 +1,61 @@
-var assert = require('chai').assert;
+const assert = require('chai').assert;
+const expect = require('chai').expect;
 
 describe('История коммитов', function() {
-  it('есть блок с "хлебными крошками"', function() {
+  it('блок с "хлебными крошками" есть и корректно отображается', function() {
     return this.browser
       .url('/')
-      .isExisting('.breadcrumbs')
-      .then(function(exists) {
-          assert.ok(exists, 'Нет "хлебных крошек"')
-      });
+      .assertExists(
+        '.breadcrumbs',
+        '"Хлебных крошек" нет на сайте'
+      )
+      .assertView('plain', '.breadcrumbs');
   });
 
   it('есть блок с контентом', function() {
     return this.browser
       .url('/')
-      .isExisting('.content')
-      .then(function(exists) {
-          assert.ok(exists, 'Нет блока с контентом')
-      });
+      .assertExists('.content', 'нет блока с контентом')
   });
 
   it('есть коммиты в блоке с контентом', function() {
     return this.browser
       .url('/')
-      .isExisting('.content .commit')
-      .then(function(exists) {
-          assert.ok(exists, 'Блок контентом пуст')
-      });
+      .assertExists('.content .commit', 'блок контентом пуст')
+  });
+});
+
+describe('Информация о коммите', function() {
+  it('есть блок с информацией', function() {
+    return this.browser
+      .url('/')
+      .assertExists('.commit:first-child .commit__info', 'нет блока с информацией о коммите')
+  });
+
+  it('есть элемент c информацией об авторе есть и он не пуст ', function() {
+    return this.browser
+      .url('/')
+      .expectTextContent('.commit:first-child  .commit__author')
+  });
+
+  it('есть элемент c информацией о дате есть и он не пуст ', function() {
+    return this.browser
+      .url('/')
+      .expectTextContent('.commit:first-child  .commit__date')
+  });
+
+  it('есть элемент с сообщением коммита и он не пуст', function() {
+    return this.browser
+      .url('/')
+      .expectTextContent('.commit:first-child .commit__msg')
+  });
+
+  it('есть элемент с ссылкой на коммит', function() {
+    return this.browser
+      .url('/')
+      .assertExists('.commit:first-child .commit__link', 'нет элемента с ссылкой на коммит')
+      .assertExists('.commit:first-child .commit__link > a')
+      .getAttribute('.commit:first-child .commit__link > a', 'href')
+      .then((href) => expect(href).not.empty)
   });
 });
