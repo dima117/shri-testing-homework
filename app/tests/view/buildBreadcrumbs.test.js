2c259752f134529aaa0d05a1abc16212768ab2b2
Unknown <maryshipkova@gmail.com>
2018-10-18 22:13:46 +0300

    rename func

--- app/tests/view/buildBreadcrumbs.test.js
+++ app/tests/view/buildBreadcrumbs.test.js
@@ -1,32 +1,32 @@
 const chai = require('chai'),
     expect = chai.expect;
-const {buildBreadcrumbs: buildBreadcrumbsTest} = require('../../view/buildBreadcrumbs');
+const { buildBreadcrumbs} = require('../../view/buildBreadcrumbs');
 
-describe('buildBreadcrumbsTest', () => {
+describe('buildBreadcrumbs', () => {
 
     it('should return an array', () => {
 
-        let result = buildBreadcrumbsTest();
+        let result = buildBreadcrumbs();
         expect(result).to.be.an('array');
     });
 
     it('should have 2 item while only hash provided', () => {
 
         const testHash = 'testHash';
-        let result = buildBreadcrumbsTest(testHash);
+        let result = buildBreadcrumbs(testHash);
         expect(result.length).to.be.equal(2);
     });
     it('should have HISTORY item with href: "/" while only hash provided', () => {
 
         const testHash = 'testHash';
-        let result = buildBreadcrumbsTest(testHash);
+        let result = buildBreadcrumbs(testHash);
         expect(result.some(element => element.text === 'HISTORY' && element.href === '/')).to.be.true;
     });
 
     it('should have ROOT item with undefined href while only hash provided', () => {
 
         const testHash = 'testHash';
-        let result = buildBreadcrumbsTest(testHash);
+        let result = buildBreadcrumbs(testHash);
         expect(result.some(element => element.text === 'ROOT' && element.href === undefined)).to.be.true;
     });
 
@@ -34,7 +34,7 @@ describe('buildBreadcrumbsTest', () => {
 
         const testHash = 'testHash',
             testPath = 'testPath';
-        let result = buildBreadcrumbsTest(testHash, testPath);
+        let result = buildBreadcrumbs(testHash, testPath);
         expect(result.length).to.be.equal(3);
     });
 
@@ -42,7 +42,7 @@ describe('buildBreadcrumbsTest', () => {
 
         const testHash = 'testHash',
             testPath = 'testPath';
-        let result = buildBreadcrumbsTest(testHash, testPath);
+        let result = buildBreadcrumbs(testHash, testPath);
         expect(result.some(element => element.text === 'HISTORY' && element.href === '/')).to.be.true;
     });
 
@@ -50,7 +50,7 @@ describe('buildBreadcrumbsTest', () => {
 
         const testHash = 'testHash',
             testPath = 'testPath';
-        let result = buildBreadcrumbsTest(testHash, testPath);
+        let result = buildBreadcrumbs(testHash, testPath);
         expect(result.some(element => element.text === 'ROOT' && element.href === `/files/${testHash}/`)).to.be.true;
     });
 
@@ -58,19 +58,19 @@ describe('buildBreadcrumbsTest', () => {
 
         const testHash = 'testHash',
             testPath = 'testPath';
-        let result = buildBreadcrumbsTest(testHash, testPath);
+        let result = buildBreadcrumbs(testHash, testPath);
         expect(result.some(element => element.text === testPath)).to.be.true;
     });
 
 
     it('should have 1 item w/o arguments', () => {
-        let result = buildBreadcrumbsTest();
+        let result = buildBreadcrumbs();
         expect(result.length).to.be.equal(1);
     });
 
     it('should have HISTORY item with undefined href w/o arguments', () => {
 
-        let result = buildBreadcrumbsTest();
+        let result = buildBreadcrumbs();
         expect(result.some(element => element.text === 'HISTORY' && element.href === undefined)).to.be.true;
     });
 });
