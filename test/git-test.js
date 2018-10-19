fda8372593c038e5dd155685e7920cc88b20716e
bercly0b <vvworkmail@yandex.ru>
2018-10-19 20:37:10 +0300

    added sinon stubs in git tests

--- test/git-test.js
+++ test/git-test.js
@@ -1,50 +1,48 @@
 const assert = require('assert')
 const chai = require('chai')
+const sinon = require('sinon')
 const { expect } = chai
 
 const { gitHistory, gitFileTree, gitFileContent } = require('../utils/git')
 
-const stabFileTree = () => Promise.resolve(`040000 tree 6a033b657f10911ad9b65c27c3f9b6fb6130b058	public \n`)
-const stabHistory = () => Promise.resolve(`38429bed94bd7c107c65fed6bffbf443ff0f4183	Dmitry Andriyanov	2018-10-15 13:22:09 +0300	заготовка приложения\n`)
-
 describe('functions from utils/git.js', () => {
 
   describe('gitHistory', () => {
+    const stub = sinon.stub()
+    stub.returns(Promise.resolve(`3	D	1	a\n3	D	1	a\n3	D	1	a\n\n\n`))
 
     it('должен вернуть промис', () => {
-      expect(gitHistory(1, 1, stabHistory)).to.be.a('promise')
+      expect(gitHistory(1, 1, stub)).to.be.a('promise')
     })
 
-    it('промис должен вернуть массив', async () => {
-      const result = await gitHistory(1, 1, stabHistory)
-      expect(result).to.be.a('array')
+    it('ключи объеков массива из промиса отфильтрованы от пустых значений', async () => {
+      const mappedArr = await gitHistory(1, 1, stub)
+      const result = mappedArr.every(({ hash, msg }) => hash && msg)
+      expect(result).to.be.true
     })
 
-    it('объекты массива должны иметь ключи hash, author, timestamp, msg', async () => {
-      const result = await gitHistory(1, 1, stabHistory)
+    it('в объектах массива из промиса должны быть ключи hash, author, timestamp, msg', async () => {
+      const result = await gitHistory(1, 1, stub)
       expect(result[0]).to.have.all.keys('hash', 'author', 'timestamp', 'msg')
     })
   })
 
   describe('gitFileTree', () => {
+    const stub = sinon.stub()
+    stub.returns(Promise.resolve(`0 t 8	p\n0 t 8	p\n0 t 8	p\n\n\n`))
 
     it('должен вернуть промис', () => {
-      expect(gitFileTree(null, null, stabFileTree)).to.be.a('promise')
+      expect(gitFileTree(null, null, stub)).to.be.a('promise')
     })
 
-    it('промис должен вернуть массив', async () => {
-      const result = await gitFileTree(null, null, stabFileTree)
-      expect(result).to.be.a('array')
+    it('ключи объеков массива из промиса отфильтрованы от пустых значений', async () => {
+      const mappedArr = await gitFileTree(null, null, stub)
+      const result = mappedArr.every(({ hash, type }) => hash && type)
+      expect(result).to.be.true
     })
 
-    it('в массиве не должно быть пустых строк', async () => {
-      const result = await gitFileTree(null, null, stabFileTree)
-      const filtred = result.filter(el => el === '')
-      expect(filtred).to.be.empty
-    })
-
-    it('объекты массива должны иметь ключи type, hash, path', async () => {
-      const result = await gitFileTree(null, null, stabFileTree)
+    it('в объектах массива из промиса должны быть ключи type, hash, path', async () => {
+      const result = await gitFileTree(null, null, stub)
       expect(result[0]).to.have.all.keys('type', 'hash', 'path')
     })
   })
