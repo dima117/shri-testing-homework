252944e1545204d5818c988af7df2a55a59d8f35
Maria Shipkova <maryshipkova@gmail.com>
2018-10-19 15:47:23 +0300

    added breadcrumbs nav test

--- hermione/simple.hermione.js
+++ hermione/simple.hermione.js
@@ -2,6 +2,7 @@ const chai = require('chai'),
     expect = chai.expect;
 
 describe('start page', () => {
+
     it('should have HISTORY breadcrumb', function () {
         return this.browser
             .url('/')
@@ -43,15 +44,18 @@ describe('start page', () => {
                 expect(text.includes('404')).to.be.false;
             });
     });
+
+
 });
 
 describe('commit page', () => {
 
-    const COMMIT_HASH = 'cc2284293758e32c50fa952da2f487c8c5e8d023';
+    const COMMIT_HASH = 'cc2284293758e32c50fa952da2f487c8c5e8d023',
+        URL = `/files/${COMMIT_HASH}/`;
 
     it('should have HISTORY/ROOT breadcrumb', function () {
         return this.browser
-            .url(`/files/${COMMIT_HASH}/`)
+            .url(URL)
             .getText('.breadcrumbs')
             .then((value) => {
                 expect(value.split(' ').join('')).to.be.equal('HISTORY/ROOT');
@@ -61,7 +65,7 @@ describe('commit page', () => {
 
     it('should have list of files', function () {
         return this.browser
-            .url(`/files/${COMMIT_HASH}/`)
+            .url(URL)
             .getText('.content ul li')
             .then((files) => {
                 expect(files).to.be.an('array');
@@ -71,13 +75,22 @@ describe('commit page', () => {
     it('should have link to file', async function () {
         let link;
         await this.browser
-            .url(`/files/${COMMIT_HASH}/`)
+            .url(URL)
             .getText('.content a').then((commits) => {
                 link = `/content/${COMMIT_HASH}/${commits[0]}`;
             });
         return this.browser.url(link).getText('.container')
             .then(text => {
                 expect(text.includes('404')).to.be.false;
+            });
+    });
+
+    it('should return to history page via breadcrumb', function () {
+        return this.browser
+            .url(URL)
+            .getHTML('.breadcrumbs a')
+            .then((text) => {
+                expect(text.match(/href="\/"/)[0]).not.to.be.equal("");
             });
     });
 });
@@ -108,5 +121,26 @@ describe('file description', () => {
             });
     });
 
+    it('should return to history page via breadcrumb', function () {
+        return this.browser
+            .url(URL)
+            .getHTML('.breadcrumbs a')
+            .then((text) => {
+                expect(text[0].match(/href="\/"/)[0]).not.to.be.equal("");
+            });
+    });
 
+    it('should return to commit page via breadcrumb', async function () {
+        let parentUrl;
+        await this.browser
+            .url(URL)
+            .getHTML('.breadcrumbs a')
+            .then((text) => {
+                parentUrl = text[1].match(/\/*\w*\/\w*\/*/)[0];
+            });
+        return this.browser.url(parentUrl).getText('.container')
+            .then(text => {
+                expect(text.includes('404')).to.be.false;
+            });
+    });
 });
