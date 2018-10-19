cd8de07994ac47fddad63ca0d3e38d71a99da9ef
Alexander Ivankov <alexanderivankoff@gmail.com>
2018-10-19 03:54:15 +0300

    Testing [homework]: написал hermione тесты на страницу с файловой структурой

--- tests/integration/commitsHistory.hermione.js
+++ tests/integration/commitsHistory.hermione.js
@@ -1,6 +1,7 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 
+
 describe('История коммитов', function() {
   it('блок с "хлебными крошками" есть и корректно отображается', function() {
     return this.browser
@@ -12,11 +13,13 @@ describe('История коммитов', function() {
       .assertView('plain', '.breadcrumbs');
   });
 
+
   it('есть блок с контентом', function() {
     return this.browser
       .url('/')
       .assertExists('.content', 'нет блока с контентом')
   });
+
 
   it('есть коммиты в блоке с контентом', function() {
     return this.browser
@@ -25,6 +28,7 @@ describe('История коммитов', function() {
   });
 });
 
+
 describe('Информация о коммите', function() {
   it('есть блок с информацией', function() {
     return this.browser
@@ -32,11 +36,13 @@ describe('Информация о коммите', function() {
       .assertExists('.commit:first-child .commit__info', 'нет блока с информацией о коммите')
   });
 
+
   it('есть элемент c информацией об авторе есть и он не пуст ', function() {
     return this.browser
       .url('/')
       .expectTextContent('.commit:first-child  .commit__author')
   });
+
 
   it('есть элемент c информацией о дате есть и он не пуст ', function() {
     return this.browser
@@ -44,11 +50,13 @@ describe('Информация о коммите', function() {
       .expectTextContent('.commit:first-child  .commit__date')
   });
 
+
   it('есть элемент с сообщением коммита и он не пуст', function() {
     return this.browser
       .url('/')
       .expectTextContent('.commit:first-child .commit__msg')
   });
+
 
   it('есть элемент с ссылкой на коммит', function() {
     return this.browser
---
+++ tests/integration/fileSystem.hermione.js
@@ -0,0 +1,56 @@
+const expect = require('chai').expect;
+
+describe('Страница с файловой системой', () => {
+  it('блок с "хлебными крошками" есть и корректно отображается', function() {
+    return this.browser
+      .url('/')
+      .click('.commit:first-child .commit__link > a')
+      .assertExists(
+        '.breadcrumbs',
+        '"Хлебных крошек" нет на сайте'
+      )
+      .assertView('plain', '.breadcrumbs');
+  });
+
+
+  it('блок с "хлебными крошками" содержит ссылку на главную страницу', function() {
+    return this.browser
+      .url('/')
+      .click('.commit:first-child .commit__link > a')
+      .assertExists(
+        '.breadcrumbs > a',
+        '"Хлебных крошек" нет на сайте'
+      )
+      .getAttribute('.breadcrumbs > a', 'href')
+      .then((href) => {
+        expect(href).not.empty;
+       })
+  });
+
+
+  it('есть блок с контентом', function() {
+    return this.browser
+      .url('/')
+      .click('.commit:first-child .commit__link > a')
+      .assertExists('.content', 'нет блока с контентом')
+  });
+
+
+  it('файлы и папки отображаются в виде не нумерованного списка', function() {
+    return this.browser
+      .url('/')
+      .click('.commit:first-child .commit__link > a')
+      .assertExists('.content > ul:first-child > li', 'нет списка файлов')
+  });
+
+
+  it('не нумерованный список файловой системы содержит в себе ссылки на содержимое', function() {
+    return this.browser
+      .url('/')
+      .click('.commit:first-child .commit__link > a')
+      .assertExists('.content > ul:first-child > li > a', 'нет ссылки на содержимое')
+      .getAttribute('.content > ul:first-child > li > a', 'href')
+      .then((href) => expect(href).not.empty)
+  });
+
+})
