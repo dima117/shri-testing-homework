e26ed53b505e7834f18376bb75867d6a1a270250
smakhl <smakhl>
2018-10-20 16:25:57 +0300

    refactor for testing

--- utils/git.js
+++ utils/git.js
@@ -3,16 +3,26 @@ const REPO = resolve('.');
 
 const { execFile } = require('child_process');
 
-function executeGit(cmd, args) {
+function executeGit(args, postprocess) {
   return new Promise((resolve, reject) => {
-    execFile(cmd, args, { cwd: REPO }, (err, stdout) => {
+    execFile('git', args, { cwd: REPO }, (err, stdout) => {
       if (err) {
         reject(err);
       }
 
-      resolve(stdout.toString());
+      if (postprocess)
+        resolve(postprocess(stdout.toString()));
+      else
+        resolve(stdout.toString());
     });
   });
+}
+
+function parseMultiline(lineParser) {
+  return (rawInput) => rawInput
+    .split('\n')
+    .filter(Boolean)
+    .map(lineParser);
 }
 
 function parseHistoryItem(line) {
@@ -26,10 +36,9 @@ function parseHistoryItem(line) {
   };
 }
 
-function gitHistory(page = 1, size = 10) {
+function gitHistory({ page = 1, size = 10, gitRunner = executeGit }) {
   const offset = (page - 1) * size;
-
-  return executeGit('git', [
+  const params = [
     'log',
     '--pretty=format:%H%x09%an%x09%ad%x09%s',
     '--date=iso',
@@ -37,12 +46,9 @@ function gitHistory(page = 1, size = 10) {
     offset,
     '-n',
     size
-  ]).then(data => {
-    return data
-      .split('\n')
-      .filter(Boolean)
-      .map(parseHistoryItem);
-  });
+  ];
+
+  return gitRunner(params, parseMultiline(parseHistoryItem));
 }
 
 function parseFileTreeItem(line) {
@@ -52,20 +58,15 @@ function parseFileTreeItem(line) {
   return { type, hash, path };
 }
 
-function gitFileTree(hash, path) {
+function gitFileTree({ hash, path, gitRunner = executeGit }) {
   const params = ['ls-tree', hash];
   path && params.push(path);
 
-  return executeGit('git', params).then(data => {
-    return data
-      .split('\n')
-      .filter(Boolean)
-      .map(parseFileTreeItem);
-  });
+  return gitRunner(params, parseMultiline(parseFileTreeItem));
 }
 
-function gitFileContent(hash) {
-  return executeGit('git', ['show', hash]);
+function gitFileContent({ hash, gitRunner = executeGit }) {
+  return gitRunner(['show', hash]);
 }
 
 module.exports = {
