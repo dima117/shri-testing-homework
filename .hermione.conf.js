896bbf829f988f84559948fd61e9df5ede43607e
Aleksey Shugaev <alek-2020@yandex.ru>
2018-10-21 23:56:08 +0300

    настроки для гермионы

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,12 +1,31 @@
 module.exports = {
-    baseUrl: 'https://yandex.ru',
-    gridUrl: 'https://0.0.0.0:4444/wd/hub',
+    baseUrl: 'http://localhost:3000',
+    gridUrl: 'http://0.0.0.0:4444/wd/hub',
+
+    sets: {
+        desktop: {
+            files: 'hermione'
+        }
+    },
 
     browsers: {
         chrome: {
             desiredCapabilities: {
                 browserName: 'chrome'
             }
+        },
+        firefox: {
+            desiredCapabilities: {
+                browserName: 'firefox'
+            }
         }
-    }
+    },
+
+    plugins: {
+        'html-reporter/hermione': {
+            path: 'hermione-html-report'
+        }
+    },
+
+    compositeImage: true
 }
