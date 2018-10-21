d1b8771f85a92461514875dc69bfc770fb1937f4
Дарья Фурзикова <dfurzikova@yandex.ru>
2018-10-21 13:02:47 +0300

    Добавила файл конфиг для гермионы

---
+++ .hermione.conf.js
@@ -0,0 +1,16 @@
+module.exports = {
+  baseUrl: 'http://localhost:3000',
+  gridUrl: 'http://localhost:4444/wd/hub',
+  browsers: {
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome'
+      }
+    }
+  },
+  plugins: {
+    'html-reporter/hermione': {
+      path: 'hermione-html-report'
+    }
+  }
+}
