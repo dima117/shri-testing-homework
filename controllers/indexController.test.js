50c7b3cc6c6d20ab71371ada406c0a6ff6e02032
Шугаев Алексей Андреевич <a.shugaev@carprice.ru>
2018-10-21 17:55:46 +0300

    тесты для indexController

---
+++ controllers/indexController.test.js
@@ -0,0 +1,28 @@
+const indexController = require('./indexController');
+const {
+    expectetArgumentInRender
+} = require('./testingContants');
+const {
+    buildFolderUrl,
+    buildBreadcrumbs
+} = require('../utils/navigation');
+const {
+    gitHistory
+} = require('../utils/git');
+
+jest.mock('../utils/git')
+jest.mock('../utils/navigation')
+
+const res = {
+    render: jest.fn(() => console.log(';lsdkjf;lskdj'))
+};
+
+const req = jest.fn()
+
+indexController(req, res)
+
+test('indexController', () => {
+
+    expect(res.render).toBeCalledTimes(1)
+    expect(res.render).toBeCalledWith('index', expectetArgumentInRender)
+})
