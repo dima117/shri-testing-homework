6c9952dac57d9930ece9e9bd6c768ec8338329d3
David Guseynov <avi2dg@gmail.com>
2018-10-21 15:38:24 +0300

    Реализованы unit-тесты

---
+++ tests/navigation.js
@@ -0,0 +1,114 @@
+const { expect } = require('chai');
+
+const { buildFolderUrl, buildFileUrl, buildBreadcrumbs } = require('../utils/navigation');
+
+describe('проверка navigation', function() {
+  it('можно получить url папки', function() {
+    // подготовка
+    const hash = 'f69c43393449afe9284f3fa20010695a5faa3849';
+    const path = 'folder1/folder2/';
+    const expectedResult = `/files/${hash}/${path}`;
+
+    // действие
+    const url = buildFolderUrl(hash, path);
+
+    // проверка
+    expect(url).to.equal(expectedResult);
+  });
+
+  it('можно получить url папки без передачи path', function() {
+    // подготовка
+    const hash = 'f69c43393449afe9284f3fa20010695a5faa3849';
+    const expectedResult = `/files/${hash}/`;
+
+    // действие
+    const url = buildFolderUrl(hash);
+
+    // проверка
+    expect(url).to.equal(expectedResult);
+  });
+
+  it('можно получить url файла', function() {
+    // подготовка
+    const hash = 'f69c43393449afe9284f3fa20010695a5faa3849';
+    const path = 'folder1/folder2/';
+    const expectedResult = `/content/${hash}/${path}`;
+
+    // действие
+    const url = buildFileUrl(hash, path);
+
+    // проверка
+    expect(url).to.equal(expectedResult);
+  });
+
+  it('можно получить хлебные крошки находясь в папке с историей', function() {
+    // подготовка
+    const hash = '';
+    const expectedResult = [
+      {
+        text: 'HISTORY',
+        href: undefined
+      }
+    ];
+
+    // действие
+    const breadcrumbs = buildBreadcrumbs(hash);
+
+    // проверка
+    expect(breadcrumbs).to.deep.equal(expectedResult);
+  });
+
+  it('можно получить хлебные крошки находясь в корневой папке', function() {
+    // подготовка
+    const hash = 'f69c43393449afe9284f3fa20010695a5faa3849';
+    const expectedResult = [
+      {
+        text: 'HISTORY',
+        href: '/'
+      },
+      {
+        text: 'ROOT',
+        href: undefined
+      }
+    ];
+
+    // действие
+    const breadcrumbs = buildBreadcrumbs(hash);
+
+    // проверка
+    expect(breadcrumbs).to.deep.equal(expectedResult);
+  });
+
+  it('можно получить хлебные крошки находясь в файле', function() {
+    // подготовка
+    const hash = 'f69c43393449afe9284f3fa20010695a5faa3849';
+    const path = 'folder1/folder2/file.js';
+    const expectedResult = [
+      {
+        text: 'HISTORY',
+        href: '/'
+      },
+      {
+        text: 'ROOT',
+        href: `/files/${hash}/`
+      },
+      {
+        text: 'folder1',
+        href: `/files/${hash}/folder1/`
+      },
+      {
+        text: 'folder2',
+        href: `/files/${hash}/folder1/folder2/`
+      },
+      {
+        text: 'file.js'
+      }
+    ];
+
+    // действие
+    const breadcrumbs = buildBreadcrumbs(hash, path);
+
+    // проверка
+    expect(breadcrumbs).to.deep.equal(expectedResult);
+  });
+});
--- utils/git.js
+++ utils/git.js
@@ -3,73 +3,75 @@ const REPO = resolve('.');
 
 const { execFile } = require('child_process');
 
-function executeGit(cmd, args) {
-  return new Promise((resolve, reject) => {
-    execFile(cmd, args, { cwd: REPO }, (err, stdout) => {
-      if (err) {
-        reject(err);
-      }
+class GitUtils {
+  constructor() {
+    this.cliExecutor = this.executeGit;
+  }
 
-      resolve(stdout.toString());
+  executeGit(cmd, args) {
+    return new Promise((resolve, reject) => {
+      execFile(cmd, args, { cwd: REPO }, (err, stdout) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(stdout.toString());
+      });
     });
-  });
+  }
+
+  parseHistoryItem(line) {
+    const [hash, author, timestamp, msg] = line.split('\t');
+
+    return {
+      hash,
+      author,
+      timestamp,
+      msg
+    };
+  }
+
+  gitHistory(page = 1, size = 10) {
+    const offset = (page - 1) * size;
+
+    return this.cliExecutor('git', [
+      'log',
+      '--pretty=format:%H%x09%an%x09%ad%x09%s',
+      '--date=iso',
+      '--skip',
+      offset,
+      '-n',
+      size
+    ]).then(data => {
+      return data
+        .split('\n')
+        .filter(Boolean)
+        .map(this.parseHistoryItem);
+    });
+  }
+
+  parseFileTreeItem(line) {
+    const [info, path] = line.split('\t');
+    const [, type, hash] = info.split(' ');
+
+    return { type, hash, path };
+  }
+
+  gitFileTree(hash, path) {
+    const params = ['ls-tree', hash];
+    path && params.push(path);
+
+    return this.cliExecutor('git', params).then(data => {
+      return data
+        .split('\n')
+        .filter(Boolean)
+        .map(this.parseFileTreeItem);
+    });
+  }
+
+  gitFileContent(hash) {
+    return this.cliExecutor('git', ['show', hash]);
+  }
 }
 
-function parseHistoryItem(line) {
-  const [hash, author, timestamp, msg] = line.split('\t');
-
-  return {
-    hash,
-    author,
-    timestamp,
-    msg
-  };
-}
-
-function gitHistory(page = 1, size = 10) {
-  const offset = (page - 1) * size;
-
-  return executeGit('git', [
-    'log',
-    '--pretty=format:%H%x09%an%x09%ad%x09%s',
-    '--date=iso',
-    '--skip',
-    offset,
-    '-n',
-    size
-  ]).then(data => {
-    return data
-      .split('\n')
-      .filter(Boolean)
-      .map(parseHistoryItem);
-  });
-}
-
-function parseFileTreeItem(line) {
-  const [info, path] = line.split('\t');
-  const [, type, hash] = info.split(' ');
-
-  return { type, hash, path };
-}
-
-function gitFileTree(hash, path) {
-  const params = ['ls-tree', hash];
-  path && params.push(path);
-
-  return executeGit('git', params).then(data => {
-    return data
-      .split('\n')
-      .filter(Boolean)
-      .map(parseFileTreeItem);
-  });
-}
-
-function gitFileContent(hash) {
-  return executeGit('git', ['show', hash]);
-}
-
-module.exports = {
-  gitHistory,
-  gitFileTree,
-  gitFileContent
-};
+module.exports = new GitUtils();
