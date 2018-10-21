42c8465ced4083e9495b493ab1c9581c8d6bfeb3
Dmitry Volovod <dimazoll@shri2018.yaconnect.com>
2018-10-21 16:46:55 +0300

    move buildObjectUrl function to navigation

--- utils/navigation.js
+++ utils/navigation.js
@@ -6,6 +6,17 @@ function buildFileUrl(parentHash, path) {
   return `/content/${parentHash}/${path}`;
 }
 
+function buildObjectUrl(parentHash, { path, type }) {
+  switch (type) {
+    case 'tree':
+      return buildFolderUrl(parentHash, path);
+    case 'blob':
+      return buildFileUrl(parentHash, path);
+    default:
+      return '#';
+  }
+}
+
 function buildBreadcrumbs(hash, path) {
   const bc = [
     {
@@ -46,7 +57,6 @@ function buildBreadcrumbs(hash, path) {
 }
 
 module.exports = {
-  buildFolderUrl,
-  buildFileUrl,
+  buildObjectUrl,
   buildBreadcrumbs
 };
