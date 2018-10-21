14757de19ba50f426343bbaaa55c266c138943cb
Дарья Фурзикова <dfurzikova@yandex.ru>
2018-10-22 01:13:16 +0300

    Модульные тесты на git.js

--- utils/git.js
+++ utils/git.js
@@ -14,7 +14,7 @@ class Git {
     }
   }
 
-  execute(cmd, args) {
+  _execute(cmd, args) {
     return new Promise((resolve, reject) => {
       this._exec(cmd, args, { cwd: REPO }, (err, stdout) => {
         if (err) {
@@ -29,7 +29,7 @@ class Git {
   history(page = 1, size = 10) {
     const offset = (page - 1) * size;
   
-    return this.execute('git', [
+    return this._execute('git', [
       'log',
       '--pretty=format:%H%x09%an%x09%ad%x09%s',
       '--date=iso',
@@ -47,7 +47,7 @@ class Git {
 
   parseHistoryItem(line) {
     const [hash, author, timestamp, msg] = line.split('\t');
-  
+
     return {
       hash,
       author,
@@ -67,7 +67,7 @@ class Git {
     const params = ['ls-tree', hash];
 
     path && params.push(path);
-    return this.execute('git', params).then(data => {
+    return this._execute('git', params).then(data => {
       return data
         .split('\n')
         .filter(Boolean)
@@ -76,9 +76,8 @@ class Git {
   }
 
   fileContent(hash) {
-    return this.execute('git', ['show', hash]);
+    return this._execute('git', ['show', hash]);
   }
-
 }
 
 module.exports = Git;
