64143d690f160fe213ff587fa0708b998cabc027
Egor Gugaev <egor@gugaevegor.ru>
2018-10-22 19:44:41 +0300

    Реализованы все интеграционные тесты

--- avadakedavra.hermione.js
+++ avadakedavra.hermione.js
@@ -1,48 +1,101 @@
 const assert = require('assert');
-// const webdriverio = require('webdriverio');
-// const options = {
-//     browsers: {
-//         chrome: {
-//             desiredCapabilities: {
-//                 browserName: 'chrome'
-//             }
-//         },
-//         firefox: {
-//             desiredCapabilities: {
-//                 browserName: 'firefox'
-//             }
-//         }
-//     }
-// };
 
-describe('Наличие и расположение траниц приложения', () => {
-    it('Главная страница присутствует, заголовок соответствует', function () {
+describe('Наличие и расположение страниц приложения', () => {
+
+    it('История коммитов - заголовок верный', function () {
         return this.browser
             .url('/')
             .getText('.breadcrumbs')
             .then((text) => {
                 assert.equal(text, 'HISTORY', 'Заголовок не совпадает или страница отсутствует');
             });
-    }); 
+    });
 
-    it('Страница коммита присутствует и заголовок соответствует', function () {
+    it('История коммитов - содержимое верное', function () {
         return this.browser
-            .url('/files/7e013ae0440ad6e91082599376a6aaebe20d2112')
+            .url('/')
+            .isExisting('.commit')
+            .then((exist) => {
+                assert.ok(exist, 'Содержимое отсутствует');
+            });
+    });
+
+    it('Файловая система - заголовок верный', function () {
+        return this.browser
+            .url('/files/7e013ae0440ad6e91082599376a6aaebe20d2112/')
             .getText('.breadcrumbs')
             .then((text) => {
-                assert.equal(text, 'HISTORY/ROOT', 'Заголовок не совпадает или страница отсутствует');
+                assert.equal(text, 'HISTORY / ROOT', 'Заголовок не совпадает или страница отсутствует');
             });
-    }); 
+    });
+
+    it('Файловая система - содержимое верное', function () {
+        return this.browser
+            .url('/files/7e013ae0440ad6e91082599376a6aaebe20d2112/')
+            .getText('.content ul li:first-child a')
+            .then((text) => {
+                assert.equal(text, '.gitignore', 'содержимое не совпадает или страница отсутствует');
+            });
+    });
+
+    it('Содержимое файла - заголовок верный', function () {
+        return this.browser
+            .url('/content/7e013ae0440ad6e91082599376a6aaebe20d2112/.gitignore')
+            .getText('.breadcrumbs')
+            .then((text) => {
+                assert.equal(text, 'HISTORY / ROOT / .gitignore', 'Заголовок не совпадает или страница отсутствует');
+            });
+    });
+
+    it('Содержимое файла - содержимое верное', function () {
+        return this.browser
+            .url('/content/7e013ae0440ad6e91082599376a6aaebe20d2112/.gitignore')
+            .getText('.file-content')
+            .then((text) => {
+                assert.equal(text, 'node_modules', 'содержимое не совпадает или страница отсутствует');
+            });
+    });
+
 });
 
+describe('Правильно работают переходы по страницам', () => {
 
-describe('Наличие и отображение файлов', () => { 
-    it('Файлы README присутствует', function () {
+    it('из списка коммитов на список файлов', function () {
         return this.browser
-            .url('/content/90180910fc27a11272a3e5caeeb119a51e5c0545/README.md')
-            .isExisting('.file-content')
-            .then((exist) => {
-                assert.ok(exist, 'Вложенные файлы отображаются с ошибкой');
+            .url('/')
+            .click('a[href$="/files/38429bed94bd7c107c65fed6bffbf443ff0f4183/"]')
+            .getText('.breadcrumbs')
+            .then((text) => {
+                assert.equal(text, 'HISTORY / ROOT', 'Переход неверный');
+            });
+    });
+    it('из списка файлов во вложенную папку', function () {
+        return this.browser
+            .url('/files/38429bed94bd7c107c65fed6bffbf443ff0f4183/')
+            .click('a[href$="/files/38429bed94bd7c107c65fed6bffbf443ff0f4183/bin"]')
+            .getText('.breadcrumbs')
+            .then((text) => {
+                assert.equal(text, 'HISTORY / ROOT / bin', 'Переход неверный');
+            });
+    });
+
+    it('Из списка файлов на страницу отдельного файла', function () {
+        return this.browser
+            .url('/files/cc2284293758e32c50fa952da2f487c8c5e8d023/')
+            .click('.content ul li:first-child a')
+            .getText('.breadcrumbs')
+            .then((text) => {
+                assert.equal(text, 'HISTORY / ROOT / .gitignore', 'Заголовок не совпадает или страница отсутствует');
+            });
+    });
+
+    it('Переход по хлебным крошкам', function () {
+        return this.browser
+            .url('/content/cc2284293758e32c50fa952da2f487c8c5e8d023/controllers/contentController.js')
+            .click('.breadcrumbs a:first-child')
+            .getText('.breadcrumbs')
+            .then((text) => {
+                assert.equal(text, 'HISTORY', 'Заголовок не совпадает или страница отсутствует');
             });
     });
 });
