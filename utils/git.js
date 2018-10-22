4e3446296574f4aeb3491aa34837187384af9358
Anna <awasome.sabrielle@ya.ru>
2018-10-22 23:10:10 +0300

    Тестовый репозиторий для интеграционных

--- utils/git.js
+++ utils/git.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
-const REPO = resolve('.');
+const REPO = (process.env.NODE_ENV === 'test') ? resolve('./test_repo') : resolve('.');
 
 const { execFile } = require('child_process');
 
