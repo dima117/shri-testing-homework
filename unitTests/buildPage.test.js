3b3d71505bce9c663c0b34b279bab75c795303bf
Evgenii Starostin <e.starostin89@gmail.com>
2018-10-23 00:01:56 +0300

    добавил и прошелся линтером

--- unitTests/buildPage.test.js
+++ unitTests/buildPage.test.js
@@ -1,28 +1,28 @@
 const { expect } = require('chai');
 const { buildIndexPage, buildFilesPage, buildContentPage } = require('../utils/buildPage');
 
-describe('формирование страниц перед рендером', function () {
-  describe('indexPage', function () {
+describe('формирование страниц перед рендером', () => {
+  describe('indexPage', () => {
     // подготовка
     const history = [
       {
-        "hash": "83736c5588bfc7ab6f25a2bf5b438ac789aa769a",
-        "author": "Evgenii Starostin",
-        "timestamp": "2018-10-18 17:19:36 +0300",
-        "msg": "изменил названия тестов"
+        hash: '83736c5588bfc7ab6f25a2bf5b438ac789aa769a',
+        author: 'Evgenii Starostin',
+        timestamp: '2018-10-18 17:19:36 +0300',
+        msg: 'изменил названия тестов',
       },
       {
-        "hash": "5d22753d2cbd326fb74c81bca51025e9ebc1d5b3",
-        "author": "Evgenii Starostin",
-        "timestamp": "2018-10-18 03:06:32 +0300",
-        "msg": "добавил firefox в интеграционные тесты"
-      }
+        hash: '5d22753d2cbd326fb74c81bca51025e9ebc1d5b3',
+        author: 'Evgenii Starostin',
+        timestamp: '2018-10-18 03:06:32 +0300',
+        msg: 'добавил firefox в интеграционные тесты',
+      },
     ];
 
     // действие
     const indexPage = buildIndexPage(history);
 
-    it('в объектах массива list есть свойство href', function () {
+    it('в объектах массива list есть свойство href', () => {
       // проверка
       expect(indexPage.list[0]).to.have.property('href');
     });
@@ -43,33 +43,33 @@ describe('формирование страниц перед рендером', 
     });
   });
 
-  describe('filesPage', function () {
+  describe('filesPage', () => {
     // подготовка
     const list = [
       {
-        "type": "blob",
-        "hash": "6e294e95c4d03a00bf72ed12348332ec3d6372db",
-        "path": "controllers/contentController.js"
+        type: 'blob',
+        hash: '6e294e95c4d03a00bf72ed12348332ec3d6372db',
+        path: 'controllers/contentController.js',
       },
       {
-        "type": "tree",
-        "hash": "6a1c8de449609cf3a6dc4a0fc2c8ba1896f54abf",
-        "path": "controllers/"
+        type: 'tree',
+        hash: '6a1c8de449609cf3a6dc4a0fc2c8ba1896f54abf',
+        path: 'controllers/',
       },
       {
-        "type": "",
-        "hash": "some_hash",
-        "path": "some_path"
+        type: '',
+        hash: 'some_hash',
+        path: 'some_path',
       },
     ];
 
     const hash = '83736c5588bfc7ab6f25a2bf5b438ac789aa769a';
-    const pathParam = ["controllers"];
+    const pathParam = ['controllers'];
 
     // действие
     const filesPage = buildFilesPage(list, hash, pathParam);
 
-    it('в объектах массива files есть свойство href', function () {
+    it('в объектах массива files есть свойство href', () => {
       // проверка
       expect(filesPage.files[0]).to.have.property('href');
     });
@@ -95,12 +95,12 @@ describe('формирование страниц перед рендером', 
     });
   });
 
-  describe('contentPage', function () {
+  describe('contentPage', () => {
     // подготовка
     const content = 'content';
 
     const hash = '83736c5588bfc7ab6f25a2bf5b438ac789aa769a';
-    const path = ["controllers", "indexController.js"];
+    const path = ['controllers', 'indexController.js'];
 
     // действие
     const contentPage = buildContentPage(content, hash, path);
