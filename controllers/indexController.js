9b0e73e25d60267f5d5ad4f7a2eec1063c0ec5bc
fox-tracks <t.andriyanova@yahoo.com>
2018-10-24 00:31:58 +0300

    add unit test for indexController

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,17 +1,26 @@
 const { gitHistory } = require('../utils/git');
 const { buildFolderUrl, buildBreadcrumbs } = require('../utils/navigation');
 
-module.exports = function(req, res) {
-  gitHistory(1, 20).then(
+
+module.exports = function(req, res, next, stubs) {
+
+  const getHistory = stubs.getFakeHistory || gitHistory;
+  const getFolderUrl = stubs.getFakeFolderUrl || buildFolderUrl;
+  const getBreadcrumbs = stubs.getFakeBreadcrumbs || buildBreadcrumbs;
+
+
+  return getHistory(1, 20)
+    .then(
     history => {
       const list = history.map(item => ({
         ...item,
-        href: buildFolderUrl(item.hash, '')
+        href: getFolderUrl(item.hash, '')
       }));
+
 
       res.render('index', {
         title: 'history',
-        breadcrumbs: buildBreadcrumbs(),
+        breadcrumbs: getBreadcrumbs(),
         list
       });
     },
