6b2ff62c28070382b5f9c9a893dade26e27988eb
Dmitry Volovod <dimazoll@shri2018.yaconnect.com>
2018-10-24 15:54:16 +0300

    refactor git utility to make it testable

--- utils/git.js
+++ utils/git.js
@@ -3,73 +3,84 @@ const REPO = resolve('.');
 
 const { execFile } = require('child_process');
 
-function executeGit(cmd, args) {
-  return new Promise((resolve, reject) => {
-    execFile(cmd, args, { cwd: REPO }, (err, stdout) => {
-      if (err) {
-        reject(err);
-      }
+class Git {
+  constructor() {
+    if (Git._instance) {
+      return Git._instance;
+    } else {
+      Git._instance = this;
+    }
+  }
 
-      resolve(stdout.toString());
+  execute(cmd, args) {
+    return new Promise((resolve, reject) => {
+      execFile(cmd, args, { cwd: REPO }, (err, stdout) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(stdout.toString());
+      });
     });
-  });
+  }
+
+  // разбирает один элемент истории, которую строчка за строчкой возвращает гит
+  static parseHistoryItem(line) {
+    const [hash, author, timestamp, msg] = line.split('\t');
+
+    return {
+      hash,
+      author,
+      timestamp,
+      msg
+    };
+  }
+
+  // Выводит историю коммитов в формате
+  history(page = 1, size = 10) {
+    const offset = (page - 1) * size;
+
+    return this.execute('git', [
+      'log',
+      '--pretty=format:%H%x09%an%x09%ad%x09%s',
+      '--date=iso',
+      '--skip',
+      offset,
+      '-n',
+      size
+    ]).then(data => {
+      return data
+        .split('\n')
+        .filter(Boolean)
+        .map(Git.parseHistoryItem);
+    });
+  }
+
+  static parseFileTreeItem(line) {
+    const [info, path] = line.split('\t');
+    const [, type, hash] = info.split(' ');
+
+    return { type, hash, path };
+  }
+
+  fileTree(hash, path) {
+    const params = ['ls-tree', hash];
+    if (path) {
+      params.push(path);
+    }
+    return this.execute('git', params).then(data => {
+      return data
+        .split('\n')
+        .filter(Boolean)
+        .map(Git.parseFileTreeItem);
+    });
+  }
+
+  fileContent(hash) {
+    return this.execute('git', ['show', hash]);
+  }
 }
+// static
+Git._instance = null;
 
-function parseHistoryItem(line) {
-  const [hash, author, timestamp, msg] = line.split('\t');
-
-  return {
-    hash,
-    author,
-    timestamp,
-    msg
-  };
-}
-
-function gitHistory(page = 1, size = 10) {
-  const offset = (page - 1) * size;
-
-  return executeGit('git', [
-    'log',
-    '--pretty=format:%H%x09%an%x09%ad%x09%s',
-    '--date=iso',
-    '--skip',
-    offset,
-    '-n',
-    size
-  ]).then(data => {
-    return data
-      .split('\n')
-      .filter(Boolean)
-      .map(parseHistoryItem);
-  });
-}
-
-function parseFileTreeItem(line) {
-  const [info, path] = line.split('\t');
-  const [, type, hash] = info.split(' ');
-
-  return { type, hash, path };
-}
-
-function gitFileTree(hash, path) {
-  const params = ['ls-tree', hash];
-  path && params.push(path);
-
-  return executeGit('git', params).then(data => {
-    return data
-      .split('\n')
-      .filter(Boolean)
-      .map(parseFileTreeItem);
-  });
-}
-
-function gitFileContent(hash) {
-  return executeGit('git', ['show', hash]);
-}
-
-module.exports = {
-  gitHistory,
-  gitFileTree,
-  gitFileContent
-};
+// Запускает на выполнение git с аргументами
+module.exports = Git;
