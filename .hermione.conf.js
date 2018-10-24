0eebbd451d7fd7668be745a6b52014dd9dce670b
Stanislav Permiakov <Stranger@Stanislavs-MacBook-Air.local>
2018-10-24 04:06:46 +0300

    added safari to hermi

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,15 +1,21 @@
 module.exports = {
     browsers: {
+        // Для тестирования на MacOS -- поменять закомменченные и раскомменченные браузеры
+        // safari: {
+        //     desiredCapabilities: {
+        //         browserName: 'safari'
+        //     }
+        // },
         chrome: {
             desiredCapabilities: {
                 browserName: 'chrome'
             }
         },
-        // firefox: {
-        //     desiredCapabilities: {
-        //         browserName: 'firefox'
-        //     }
-        // }
+        firefox: {
+            desiredCapabilities: {
+                browserName: 'firefox'
+            }
+        }
     },
     plugins: {
         'html-reporter/hermione': {
