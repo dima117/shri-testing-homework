96f6064cc9d82a235eeadef797b80500bcf18a02
Ivan Dashkevich <idashkevich@lement.pro>
2018-10-25 10:41:29 +0300

    Тестирование скриншотами

--- test/integrational/content.hermione.js
+++ test/integrational/content.hermione.js
@@ -6,6 +6,7 @@ describe('Содержимое страниц', () => {
     it('История коммитов содержит коммиты', async function() {
         const exists = await this.browser
             .url('/')
+            .assertView('plain', '.commit')
             .isExisting('.commit');
 
         expect(exists).to.be.true;
@@ -14,6 +15,7 @@ describe('Содержимое страниц', () => {
     it('Коммит содержит автора', async function() {
         const exists = await this.browser
             .url('/')
+            .assertView('plain', '.commit__author')
             .isExisting('.commit__author');
         
         expect(exists).to.be.true;
@@ -22,6 +24,7 @@ describe('Содержимое страниц', () => {
     it('Коммит содержит дату', async function() {
         const exists = await this.browser
             .url('/')
+            .assertView('plain', '.commit__date')
             .isExisting('.commit__date');
             
         expect(exists).to.be.true;
@@ -30,6 +33,7 @@ describe('Содержимое страниц', () => {
     it('Коммит содержит сообщение', async function() {
         const exists = await this.browser
             .url('/')
+            .assertView('plain', '.commit__msg')
             .isExisting('.commit__msg');
             
         expect(exists).to.be.true;
@@ -39,6 +43,7 @@ describe('Содержимое страниц', () => {
         const exists = await this.browser
             .url('/')
             .click(lastCommitLink)
+            .assertView('plain', '.content')
             .isExisting('.content');
         
         expect(exists).to.be.true;
@@ -51,6 +56,7 @@ describe('Содержимое страниц', () => {
             .url('/')
             .click(lastCommitLink) 
             .click(fileLink) 
+            .assertView('plain', '.file-content')
             .isExisting('.file-content');
 
         expect(exists).to.be.true;
