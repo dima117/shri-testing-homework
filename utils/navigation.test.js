529ad5ea9a9644a38ff1bcfded141bf359fdf8b7
Hope R <nadezhda.lkn@yahoo.com>
2018-10-27 12:42:31 +0300

    Make  navigartion test functions asynchronous

--- utils/navigation.test.js
+++ utils/navigation.test.js
@@ -5,7 +5,7 @@ const {
 } = require('./navigation.js')
 
 describe('Работа "хлебных крошек"', () => {
-    it('При заданных хэше и пути, функция возвращает массив из "хлебных крошек" со свойствами text и [href - optional]', () => {
+    it('При заданных хэше и пути, функция возвращает массив из "хлебных крошек" со свойствами text и [href - optional]', async () => {
         const hash = '68d14a5c95d67e815d5aa9e2560b16b902b402d9'
         const path = 'README.md'
         const expectedResult = [
@@ -17,39 +17,39 @@ describe('Работа "хлебных крошек"', () => {
             { text: 'README.md' }
         ]
 
-        const actualResult = buildBreadcrumbs(hash, path)
+        const actualResult = await buildBreadcrumbs(hash, path)
 
         expect(actualResult).toEqual(expectedResult)
     })
 
-    it('При вызове без параметров, возвращается массив из одного объекта history, у которого свойство href: undefined', async function() {
+    it('При вызове без параметров, возвращается массив из одного объекта history, у которого свойство href: undefined', async () => {
         const expectedResult = [{ text: 'HISTORY', href: undefined }]
 
-        const actualResult = buildBreadcrumbs()
+        const actualResult = await buildBreadcrumbs()
 
         expect(actualResult).toEqual(expectedResult)
     })
 })
 
 describe('Построение пути к файлам и файловым системам', () => {
-    it('При заданном хэше возвращается путь к системе файлов коммита', () => {
+    it('При заданном хэше возвращается путь к системе файлов коммита', async () => {
         const hash = '1f5487e5c51504cf2f2db00227dd815664b6d8d7'
         const path = ''
         const expectedResult =
             '/files/1f5487e5c51504cf2f2db00227dd815664b6d8d7/'
 
-        const actualResult = buildFolderUrl(hash, path)
+        const actualResult = await buildFolderUrl(hash, path)
 
         expect(actualResult).toEqual(expectedResult)
     })
 
-    it('При заданном хэше и пути возвращается путь к содежимому отдельного файла', function() {
+    it('При заданном хэше и пути возвращается путь к содежимому отдельного файла', async () => {
         const hash = '1f5487e5c51504cf2f2db00227dd815664b6d8d7'
         const path = '.hermione.conf.js'
         const expectedResult =
             '/content/1f5487e5c51504cf2f2db00227dd815664b6d8d7/.hermione.conf.js'
 
-        const actualResult = buildFileUrl(hash, path)
+        const actualResult = await buildFileUrl(hash, path)
 
         expect(actualResult).toEqual(expectedResult)
     })
